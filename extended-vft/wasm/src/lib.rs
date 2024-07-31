6378cbe85a69843222c91c713020692b1e2013f8
LouiseMedova <luisa.medova@gmail.com>
2024-07-31 15:50:16 +0300

    fmt fix

--- extended-vft/wasm/src/lib.rs
+++ extended-vft/wasm/src/lib.rs
@@ -2,6 +2,5 @@
 include!(concat!(env!("OUT_DIR"), "/extended_vft_client.rs"));
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-
 #[cfg(target_arch = "wasm32")]
 pub use extended_vft_app::wasm::*;
