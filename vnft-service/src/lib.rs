f9b494281158f77d7bb314c4cdcb4a059f584f14
MedovTimur <medov.tr@phystech.edu>
2024-11-21 09:42:20 +0300

    Adding query to vnft-service

--- vnft-service/src/lib.rs
+++ vnft-service/src/lib.rs
@@ -159,4 +159,13 @@ impl Service {
         let storage = Storage::get();
         &storage.symbol
     }
+    pub fn tokens_for_owner(&self, owner: ActorId) -> Vec<TokenId> {
+        Storage::get()
+            .tokens_for_owner
+            .get(&owner)
+            .unwrap_or(&HashSet::new())
+            .iter()
+            .cloned()
+            .collect()
+    }
 }
