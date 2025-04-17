e75c83c77c9cd7aa98898fd11837ed26629a73e4
MedovTimur <medov.tr@phystech.edu>
2025-04-17 12:41:52 +0300

    changed path to wasm/idl

--- extended-vnft/tests/test.rs
+++ extended-vnft/tests/test.rs
@@ -19,7 +19,7 @@ async fn test_basic_function() {
 
     let code_id = program_space
         .system()
-        .submit_code_file("../target/wasm32-unknown-unknown/release/extended_vnft.opt.wasm");
+        .submit_code_file("../target/wasm32-gear/release/extended_vnft.opt.wasm");
 
     let extended_vnft_factory = Factory::new(program_space.clone());
     let extended_vnft_id = extended_vnft_factory
@@ -134,7 +134,7 @@ async fn test_grant_role() {
 
     let code_id = program_space
         .system()
-        .submit_code_file("../target/wasm32-unknown-unknown/release/extended_vnft.opt.wasm");
+        .submit_code_file("../target/wasm32-gear/release/extended_vnft.opt.wasm");
 
     let extended_vft_factory = Factory::new(program_space.clone());
     let extended_vft_id = extended_vft_factory
